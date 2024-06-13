c10514182a1cb93a8a40565ba0fd9098b792906b
Taufiqu <hafizhtr24@gmail.com>
2024-06-13 20:54:02 +0700

    commit

--- pages/tentang.php
+++ pages/tentang.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Berita</title>
+    <title>Tentang</title>
 
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
